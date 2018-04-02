c325d5eb469f74b0a4a8f6b43f48473a4cb533c3
Vibhanshu Rai <vibhanshu09@outlook.com>
2018-04-02 13:51:25 +0530

    Fragment change on nav drawer option selection (begin)

--- app/src/main/java/com/example/tcsexam/trainingday3/MyToolbarActivity2.java
+++ app/src/main/java/com/example/tcsexam/trainingday3/MyToolbarActivity2.java
@@ -1,12 +1,18 @@
 package com.example.tcsexam.trainingday3;
 
+
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MyToolbarActivity2 extends AppCompatActivity {
@@ -25,6 +31,30 @@ public class MyToolbarActivity2 extends AppCompatActivity {
         drawerLayout = findViewById(R.id.drawer);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.drawer_open,R.string.drawer_colse);
         drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                int itemId = item.getItemId();
+                if (itemId == R.id.home_nav_menu)
+                    fragment = new HomeFragment();
+                else if (itemId == R.id.share_nav_menu)
+                    fragment = new ShareFragment();
+                else if (itemId == R.id.about_nav_menu)
+                    fragment = new AboutFragment();
+
+                Toast.makeText(MyToolbarActivity2.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.frame,fragment);
+                fragmentTransaction.commit();
+                getSupportActionBar().setTitle(item.getTitle());
+
+
+                return true;
+            }
+        });
 
     }
 
